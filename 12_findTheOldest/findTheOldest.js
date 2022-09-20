18046508c37970a9a1172b1f3a1b0630d380a3df
TwoStarLightMints <mondo.beyondo3@gmail.com>
2022-09-20 19:24:53 -0400

    Completed up to 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(oldPeople) {
+  let oldest = '';
 
+  for (let i = 1; i < oldPeople.length; i++) {
+    if ((oldPeople[i-1].yearOfDeath - oldPeople[i-1].yearOfBirth) < (oldPeople[i-1].yearOfDeath - oldPeople[i-1].yearOfBirth)) {
+      oldest = oldPeople[i];
+    } else {
+      oldest = oldPeople[i-1];
+    }
+  }
+  
+  return oldest;
 };
 
 // Do not edit below this line
